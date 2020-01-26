b94561ed499b0ac5a3e6f3ae51e09c8a4e01f50a
Andy Bui <abui27@ucmerced.edu>
2020-01-25 23:35:09 -0800

    Find numbers with even amount of integers

--- EvenNumberDigits.js
+++ EvenNumberDigits.js
@@ -1,13 +1,17 @@
+// Leetcode question #1295: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var findNumbers = function(nums) {
-    var evenNum = 0;
-    for(var i = 0; i < nums.length; i++) {
+    let evenNum = 0;
+    for(let i = 0; i < nums.length; i++) {
         if(nums[i].toString().length % 2 == 0) {
-            evenNum++;
+            evenNum += 1;
         }
     }
     return evenNum;
 };
+
+console.log(findNumbers([12,345,2,6,7896]))
