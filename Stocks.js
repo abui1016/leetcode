dce3ee7ed0ed64f1d890b2d4fa1f660dc2e1bf92
Andy Bui <abui27@ucmerced.edu>
2020-10-14 22:59:01 -0700

    Best time to buy and sell stocks

---
+++ Stocks.js
@@ -0,0 +1,24 @@
+// Leetcode Question #121: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+var maxProfit = function(prices) {
+
+    let buy = Number.MAX_VALUE; 
+    let profit = 0;
+
+    for(let i = 0; i < prices.length; i++) {
+        if(buy > prices[i]) {
+            buy = prices[i]; 
+        }
+        if(prices[i] - buy > profit) {
+            profit = prices[i] - buy;
+        }
+    }
+    return profit;
+}
+
+console.log(maxProfit([7, 6, 4, 3, 1])); // 0
